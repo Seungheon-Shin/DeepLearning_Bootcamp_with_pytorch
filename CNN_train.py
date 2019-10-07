d4bebd274bcb32981810b98d24246c591d28fcb0
Noel <36836739+NoelShin@users.noreply.github.com>
2019-10-07 20:26:14 +0900

    Update CNN_train.py

--- CNN_train.py
+++ CNN_train.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import os
 
-class CNN(nn.Module): # CNN 클래스 정의
+class CNN(nn.Module): # CNN 클래스 정의 및 nn.Module 클래스를 상속
     def __init__(self):
-        super(CNN, self).__init__() # nn.Module 클래스를 상속
+        super(CNN, self).__init__() # nn.Module 클래스에 있는 __init__() 메소드를 실행.
         self.input_layer = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)  # 16x28x28
         self.layer_1 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1, stride=2)  # 32x14x14
         self.layer_2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1, stride=2)  # 64x7x7
@@ -28,7 +28,7 @@ class CNN(nn.Module): # CNN 클래스 정의
 
 if __name__ == '__main__': # 이 파일을 직접 실행할때만 True값 리턴
     dataset = MNIST(root='./datasets', train=True, transform=ToTensor(), download=True) # MNIST 데이터셋 다운로드
-    data_loader = DataLoader(dataset, batch_size=32, shuffle=True) #pytorch DataLoader 모듈 이용하여 데이터셋 불러오기
+    data_loader = DataLoader(dataset, batch_size=32, shuffle=True) #pytorch DataLoader 모듈 이용하여 데이터셋을 for 구문에서 돌림.
 
     model = CNN() #모델 정의
 
@@ -51,7 +51,7 @@ if __name__ == '__main__': # 이 파일을 직접 실행할때만 True값 리턴
             output = model(input)  # 32x10
             loss = criterion(output, label)  # 1
 
-            optim.zero_grad() #optimizer gradient 초기화
+            optim.zero_grad() #optimizer를 이용하여 weight들의 gradient 초기화
             loss.backward() # loss값에 따른 gradient 전달
             optim.step() #optimizer를 통한 weight 갱신
             list_loss.append(loss.detach().item()) #loss값 저장
@@ -62,7 +62,7 @@ if __name__ == '__main__': # 이 파일을 직접 실행할때만 True값 리턴
 
 
 
-    weight_dict = {'model_weight': model.state_dict(), 'adam_weight': optim.state_dict()} # 저장시킬 데이터 정의
+    weight_dict = {'model_weight': model.state_dict(), 'adam_weight': optim.state_dict()} # 저장시킬 가중치 정의
     torch.save(weight_dict, "./weight_dict.pt") #훈련된 weight 저장
 
 
